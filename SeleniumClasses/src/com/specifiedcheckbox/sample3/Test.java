ee9541b27ce8839680c4f74830927a78757cdfc6
IMROSE PASHA <IMROSE PASHA@DESKTOP-J3PJP1I>
2018-08-26 17:27:28 +0530

    Commit2

--- SeleniumClasses/src/com/specifiedcheckbox/sample3/Test.java
+++ SeleniumClasses/src/com/specifiedcheckbox/sample3/Test.java
@@ -6,19 +6,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
-public static void main(String[] args) {
-System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
-	
-	WebDriver driver = new ChromeDriver();
-	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-	driver.manage().window().maximize();
-	driver.get("file:///C:/Users/IMROSE%20PASHA/Desktop/HTML%20programs/Program7.html");
-	checkboxespagesPO linkpo = new checkboxespagesPO(driver);
-	System.out.println(linkpo.allChkBxSize());
-	linkpo.getAllChkBx(0).click();
-	linkpo.getAllChkBx(1).click();
-	linkpo.getAllChkBx(2).click();
-	
-	
-}
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		System.out.println("Now the Chrome Browser Launches & gets maximize");
+		driver.manage().window().maximize();
+		driver.get("file:///C:/Users/IMROSE%20PASHA/Desktop/HTML%20programs/Program7.html");
+		checkboxespagesPO linkpo = new checkboxespagesPO(driver);
+		System.out.println(linkpo.allChkBxSize());
+		linkpo.getAllChkBx(0).click();
+		linkpo.getAllChkBx(1).click();
+		linkpo.getAllChkBx(2).click();
+
+	}
 }
